db05bf908eaf6ff8062d41e6e77cc2e3c5871d5a
Mohamed Fadl <mohamed.fadl.farag@gmail.com>
2024-12-23 07:43:11 +0200

    feat: implement AppAccordion and AppAccordionPanel components with multiple panel support

--- src/components/AppAccordion.vue
+++ src/components/AppAccordion.vue
@@ -1,4 +1,64 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, provide, readonly } from 'vue'
+
+const props = defineProps<{
+  multiple: boolean
+}>()
+
+const panels = ref<string[]>([])
+
+const activePanels = defineModel<string | string[]>()
+
+function registerPanel(title: string) {
+  // check if the panel is already registered
+  if (panels.value.includes(title)) {
+    console.warn(`Panel with title "${title}" is already registered`)
+    return
+  }
+
+  panels.value.push(title)
+}
+
+// check if the panel is already active should be toggled, if not, add it to the activePanels
+function togglePanel(title: string) {
+  if (props.multiple) {
+    // check if activePanels is undefined or empty
+    if (!activePanels.value || activePanels.value.length === 0) {
+      activePanels.value = [title]
+      return
+    }
+
+    // check if the panel is already active
+    if (activePanels.value.includes(title)) {
+      if (Array.isArray(activePanels.value)) {
+        activePanels.value = activePanels.value.filter((panel) => panel !== title)
+      }
+    } else {
+      activePanels.value = [...activePanels.value, title]
+    }
+  } else {
+    activePanels.value = title
+  }
+}
+
+provide(injectionKey, {
+  withinAccordion: true,
+  registerPanel,
+  activePanels: readonly(activePanels),
+  togglePanel,
+  multiple: props.multiple,
+})
+</script>
+<script lang="ts">
+import type { InjectionKey, Ref } from 'vue'
+export const injectionKey = Symbol('AppAccordion') as InjectionKey<{
+  withinAccordion: boolean
+  registerPanel: (title: string) => void
+  activePanels: Readonly<Ref<string | readonly string[] | undefined>>
+  togglePanel: (title: string) => void
+  multiple: boolean
+}>
+</script>
 
 <template>
   <div>
--- src/components/AppAccordionPanel.vue
+++ src/components/AppAccordionPanel.vue
@@ -1,15 +1,44 @@
 <script setup lang="ts">
-defineProps<{
+import { inject, ref, readonly, computed } from 'vue'
+import { injectionKey } from './AppAccordion.vue'
+
+const props = defineProps<{
   title: string
 }>()
+
+const accordionProvider = inject(injectionKey, {
+  withinAccordion: false,
+  registerPanel: () => {},
+  activePanels: readonly(ref<string | string[]>()),
+  togglePanel: () => {},
+  multiple: false,
+})
+
+if (!accordionProvider.withinAccordion) {
+  throw new Error('AppAccordionPanel must be used within an AppAccordion')
+}
+
+const isActive = computed(() => {
+  // if multiple, check if the activePanels array includes the title
+  if (accordionProvider.multiple) {
+    return Array.isArray(accordionProvider.activePanels.value)
+      ? accordionProvider.activePanels.value.includes(props.title)
+      : false
+  }
+
+  return accordionProvider.activePanels.value === props.title
+})
 </script>
 
 <template>
   <div class="bg-base-200 rounded-lg mb-4">
-    <div class="p-4 text-xl font-medium">
+    <div
+      class="p-4 text-xl font-medium cursor-pointer"
+      @click="accordionProvider.togglePanel(title)"
+    >
       {{ title }}
     </div>
-    <div class="p-4">
+    <div class="p-4" v-if="isActive">
       <slot></slot>
     </div>
   </div>
